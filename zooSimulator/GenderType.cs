948694fc3014b42213e825af3fa1f68c1d2e4eda
duc <hohuynhduca7bh123@gmail.com>
2020-08-23 11:52:02 +0700

    create some class

---
+++ zooSimulator/GenderType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zooSimulator
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Neural
+    }
+}
---
+++ zooSimulator/Species.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zooSimulator
+{
+    public abstract class Species
+    {
+        private string id;
+        private string name;
+        private double heath;
+        private double length;
+        private double weight;
+        private GenderType gender;
+        private int age;
+        private int breedingPriod;
+
+        protected string Id { get => id; set => id = value; }
+        protected string Name { get => name; set => name = value; }
+        protected double Heath { get => heath; set => heath = value; }
+        protected double Length { get => length; set => length = value; }
+        protected double Weight { get => weight; set => weight = value; }
+        protected GenderType Gender { get => gender; set => gender = value; }
+        protected int Age { get => age; set => age = value; }
+        protected int BreedingPriod { get => breedingPriod; set => breedingPriod = value; }
+
+    
+    }
+}
